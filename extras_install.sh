7502ba2eef451334445c5faa83cb42fde8c8612f
Deepanshu Yadav <rohantheyadav@gmail.com>
2021-07-28 14:21:58 +0530

    converted if to a case statement

--- extras_install.sh
+++ extras_install.sh
@@ -7,8 +7,18 @@ echo "Platform is ${os_name}, Version: ${os_version}"
 ID=$(cat /etc/os-release | grep -w ID | cut -f2 -d"=")
 
 if [ ${ID} != "mendel" ]; then
-  if [ "$os_name" = *"Red Hat"* ] || [ "$os_name" = *"CentOS"* ]; then
+
+ case $os_name in
+
+  *"Red Hat"*)
     source scl_source enable rh-python36
-  fi
+    ;;
+
+  *"CentOS"*)
+    source scl_source enable rh-python36
+    ;;
+
+  esac
+
   python3 -m pip install pandas==1.0.1
 fi
